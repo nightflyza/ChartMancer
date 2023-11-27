bbd0e169ef11d6b8f8dcd4cc886f84c194b12088
Nightfly <nightfly@email.ua>
2023-11-27 23:26:38 +0200

    tryin

--- examples/basic.php
+++ examples/basic.php
@@ -1,9 +1,11 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
-//require_once('src/api.chartmancer.php');
 
-use ChartMancer;
+//use ChartMancer;
 
-$chartMancer = new ChartMancer();
-print_r($chartMancer);
+//$chartMancer = new ChartMancer\ChartMancer();
+
+//print_r($chartMancer);
+
+print_r(get_declared_classes());
